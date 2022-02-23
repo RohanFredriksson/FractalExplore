590545daa8a4f3b0b7ec8e537415cc7009f76919
RohanFredriksson <rfre3327@uni.sydney.edu.au>
2022-02-23 13:52:28 +1100

    Cleaned repository and improved build script.

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
 cmake . -G "MinGW Makefiles"
 mingw32-make
+move FractalExplore.exe bin\FractalExplore.exe
+del CMakeCache.txt /s /f /q
+del cmake_install.cmake /s /f /q
+del Makefile /s /f /q
+@RD /S /Q ".\CMakeFiles"
