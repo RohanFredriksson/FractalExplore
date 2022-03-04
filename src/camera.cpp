177975ffce0c780e249711bb8bb59f1b97ab57d1
RohanFredriksson <rfre3327@uni.sydney.edu.au>
2022-03-04 23:25:06 +1100

    Inverted zoom.

--- src/camera.cpp
+++ src/camera.cpp
@@ -12,7 +12,7 @@ Camera::Camera(glm::vec2 pos) {
     this->view = glm::mat4(1.0f);
     this->inverseProjection = glm::mat4(1.0f);
     this->inverseView = glm::mat4(1.0f);
-    this->zoom = 0.5f;
+    this->zoom = 2.0f;
     adjustProjection();
 
 }
@@ -20,15 +20,15 @@ Camera::Camera(glm::vec2 pos) {
 void Camera::adjustProjection() {
     
     //this->projectionSize.x = this->projectionSize.y * Window::getAspectRatio();   
-    this->projection = glm::ortho(0.0f, this->projectionSize.x * this->zoom, 0.0f, this->projectionSize.y * this->zoom, 0.0f, 100.0f);
+    this->projection = glm::ortho(0.0f, this->projectionSize.x / this->zoom, 0.0f, this->projectionSize.y / this->zoom, 0.0f, 100.0f);
     this->inverseProjection = glm::inverse(this->projection);
 
 }
         
 glm::mat4 Camera::getView() {
     
-    glm::vec3 cameraPos(this->position.x - this->projectionSize.x / 2 * this->zoom, this->position.y - this->projectionSize.y / 2 * this->zoom, 20.0f);
-    glm::vec3 cameraFront(this->position.x - this->projectionSize.x / 2 * this->zoom, this->position.y - this->projectionSize.y / 2 * this->zoom, -1.0f);
+    glm::vec3 cameraPos(this->position.x - this->projectionSize.x / (2 * this->zoom), this->position.y - this->projectionSize.y / (2 * this->zoom), 20.0f);
+    glm::vec3 cameraFront(this->position.x - this->projectionSize.x / (2 * this->zoom), this->position.y - this->projectionSize.y / (2 * this->zoom), -1.0f);
     glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
     this->view = glm::lookAt(cameraPos, cameraFront, cameraUp);
 
