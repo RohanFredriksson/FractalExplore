2e93b3459651d0d00c4cf8a08adcaa404c04b114
RohanFredriksson <rfre3327@uni.sydney.edu.au>
2022-03-06 18:00:54 +1100

    Dependencies added.

---
+++ dependencies/glfw/src/CMakeFiles/glfw.dir/win32_joystick.c.obj.d
@@ -0,0 +1,40 @@
+dependencies/glfw/src/CMakeFiles/glfw.dir/win32_joystick.c.obj: \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\win32_joystick.c \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\internal.h \
+ c:\users\rohan\desktop\fractalexplore\dependencies\glfw\include\glfw\glfw3.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\stdint.h \
+ c:\mingw\include\stdint.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h c:\mingw\include\features.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\platform.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\null_platform.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\win32_platform.h \
+ c:\mingw\include\wctype.h c:\mingw\include\windows.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\stdarg.h \
+ c:\mingw\include\windef.h c:\mingw\include\winnt.h \
+ c:\mingw\include\winerror.h c:\mingw\include\string.h \
+ c:\mingw\include\basetsd.h c:\mingw\include\pshpack4.h \
+ c:\mingw\include\poppack.h c:\mingw\include\wincon.h \
+ c:\mingw\include\winbase.h c:\mingw\include\wingdi.h \
+ c:\mingw\include\winuser.h c:\mingw\include\dbt.h \
+ c:\mingw\include\winnls.h c:\mingw\include\winver.h \
+ c:\mingw\include\winnetwk.h c:\mingw\include\winreg.h \
+ c:\mingw\include\reason.h c:\mingw\include\winsvc.h \
+ C:/Users/Rohan/Desktop/FractalExplore/dependencies/glfw/deps/mingw/dinput.h \
+ c:\mingw\include\objbase.h c:\mingw\include\rpc.h \
+ c:\mingw\include\rpcdce.h c:\mingw\include\basetyps.h \
+ c:\mingw\include\rpcdcep.h c:\mingw\include\rpcnsi.h \
+ c:\mingw\include\rpcnterr.h c:\mingw\include\rpcndr.h \
+ c:\mingw\include\rpcnsip.h c:\mingw\include\objfwd.h \
+ c:\mingw\include\stdlib.h c:\mingw\include\errno.h \
+ c:\mingw\include\wtypes.h c:\mingw\include\unknwn.h \
+ c:\mingw\include\objidl.h c:\mingw\include\cguid.h \
+ C:/Users/Rohan/Desktop/FractalExplore/dependencies/glfw/deps/mingw/_mingw_dxhelper.h \
+ C:/Users/Rohan/Desktop/FractalExplore/dependencies/glfw/deps/mingw/xinput.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\null_joystick.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\win32_joystick.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\win32_thread.h \
+ C:\Users\Rohan\Desktop\FractalExplore\dependencies\glfw\src\win32_time.h \
+ c:\mingw\include\stdio.h c:\mingw\include\sys/types.h \
+ c:\mingw\include\math.h
